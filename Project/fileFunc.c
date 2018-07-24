465a4c3c3d4a5bff70552a7ef3b7a4c9620f4eeb
roypel <31568986+roypel@users.noreply.github.com>
2018-07-24 12:34:02 +0300

    Add files via upload

--- Project/fileFunc.c
+++ Project/fileFunc.c
@@ -8,28 +8,30 @@
 void fillBoard(gameState *metaBoard, FILE* ifp) {
 	int input, i, j, filled = 0;
 	char *c, string[4] = { 0 };/*USING THE 5*5 LIMITATION, MAYBE USE CALLOC WITH log_10 instead*/
+	/*c used to check each char in a cell, string used to read a cell (number + fixed + \0)*/
 	freeBoard(metaBoard->gameBoard);
-	fscanf(ifp, "%d", &input);
+	fscanf(ifp, "%d", &input); /*Read block size*/
 	metaBoard->gameBoard->cols = metaBoard->cols = input;
 	filled += input;
 	fscanf(ifp, "%d", &input);
 	metaBoard->gameBoard->rows = metaBoard->rows = input;
 	filled *= input;
+	filled *= filled; /*Number of possible filled cells is block size squared*/
 	removeAllNext(metaBoard->moves->firstNode->next);/*Clear Undo/Redo list*/
 	initalizeBoard(metaBoard->gameBoard);
 	for (i = 0; i < metaBoard->rows * metaBoard->cols; i++) {
 		for (j = 0; j < metaBoard->cols * metaBoard->rows; j++) {
-			fscanf(ifp, "%s", string);
-			c = string;
-			if (*c == '0') {
+			fscanf(ifp, "%s", string); /*Read next cell*/
+			c = string; /*Questionable implementation, maybe can replace string with c*/
+			if (*c == '0') {/*Cell is empty, filled decrease and not fixed (default 0)*/
 				filled--;
 			} else {
-				while (c != '\0') {
-					if (*c == '.') {
+				while (c != '\0') {/*Read until end of cell*/
+					if (*c == '.') {/*Cell is fixed*/
 						metaBoard->gameBoard->board[i][j].fixed = 1;
 					} else {
-						metaBoard->gameBoard->board[i][j].value *= 10
-								+ (*c - '0');
+						metaBoard->gameBoard->board[i][j].value *= 10;/*Insert another digit*/
+						metaBoard->gameBoard->board[i][j].value += (*c - '0');
 					}
 					c++;
 				}
@@ -37,6 +39,7 @@ void fillBoard(gameState *metaBoard, FILE* ifp) {
 		}
 	}
 }
+
 
 void saveFile(gameState *metaBoard, char *fileName) {
 	FILE *ifp;
