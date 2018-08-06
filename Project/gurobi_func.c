7d8be31e41403fb14183da8c8093da0da840d936
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-08-06 11:03:28 +0300

    Add files via upload

---
+++ Project/gurobi_func.c
@@ -0,0 +1,176 @@
+/* Copyright 2018, I-Lakunishok-Peleg, Inc. */
+
+/* This example formulates and solves the following simple MIP model:
+
+ maximize    x +   y + 2 z
+ subject to  x + 2 y + 3 z <= 4
+ x +   y       >= 1
+ x, y, z binary
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "gurobi_c.h"
+
+int fillboard(int cols, int rows, int** filled) {/*return -1 on failure,1/integer on success*/
+	/*also need to return the solution of the filled board,so find howto optimally*/
+	int i,j,k;
+	GRBenv *env = NULL;
+	GRBmodel *model = NULL;
+	int error = 0;
+	int* sol;
+	int* ind;
+	int* val;
+	int obj[1]={0};
+	char* vtype;
+	int optimstatus;
+	double objval;
+
+	sol=(int*)calloc(cols*rows*cols*rows*cols*rows,sizeof(int));
+	ind=(int*)calloc(cols*rows,sizeof(int));
+	val=(int*)calloc(cols*rows,sizeof(int));
+	vtype=(char*) calloc(cols*rows*cols*rows*cols*rows);
+
+	/* Create environment - log file is mip1.log */
+	error = GRBloadenv(&env, "mip1.log");
+	if (error) {
+		printf("ERROR %d GRBloadenv(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Create an empty model named "mip1" */
+	error = GRBnewmodel(env, &model, "mip1", 0, NULL, NULL, NULL, NULL, NULL );
+	if (error) {
+		printf("ERROR %d GRBnewmodel(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Add variables */
+
+
+	/* set the variables to be binary */
+	for(i=0;i<cols*rows;i++){
+		for(j=0;i<cols*rows;j++){
+			for(k=0;k<cols*rows;k++){
+				vtype[i][j][k]=GRB_BINARY;
+			}
+		}
+	}
+
+	/* add variables to model */
+	error = GRBaddvars(model, 3, 0, NULL, NULL, NULL, obj, NULL, NULL, vtype,
+			NULL );
+	if (error) {
+		printf("ERROR %d GRBaddvars(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Change objective sense to maximization */
+	error = GRBsetintattr(model, GRB_INT_ATTR_MODELSENSE, GRB_MAXIMIZE);
+	if (error) {
+		printf("ERROR %d GRBsetintattr(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* update the model - to integrate new variables */
+
+	error = GRBupdatemodel(model);
+	if (error) {
+		printf("ERROR %d GRBupdatemodel(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* First constraint: x + 2 y + 3 z <= 4 */
+
+	/* variables x,y,z (0,1,2) */
+	ind[0] = 0;
+	ind[1] = 1;
+	ind[2] = 2;
+	/* coefficients (according to variables in "ind") */
+	val[0] = 1;
+	val[1] = 2;
+	val[2] = 3;
+
+	/* add constraint to model - note size 3 + operator GRB_LESS_EQUAL */
+	/* -- equation value (4.0) + unique constraint name */
+	error = GRBaddconstr(model, 3, ind, val, GRB_LESS_EQUAL, 4.0, "c0");
+	if (error) {
+		printf("ERROR %d 1st GRBaddconstr(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Second constraint: x + y >= 1 */
+	ind[0] = 0;
+	ind[1] = 1;
+	val[0] = 1;
+	val[1] = 1;
+
+	/* add constraint to model - note size 2 + operator GRB_GREATER_EQUAL */
+	/* -- equation value (1.0) + unique constraint name */
+	error = GRBaddconstr(model, 2, ind, val, GRB_GREATER_EQUAL, 1.0, "c1");
+	if (error) {
+		printf("ERROR %d 2nd GRBaddconstr(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Optimize model - need to call this before calculation */
+	error = GRBoptimize(model);
+	if (error) {
+		printf("ERROR %d GRBoptimize(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Write model to 'mip1.lp' - this is not necessary but very helpful */
+	error = GRBwrite(model, "mip1.lp");
+	if (error) {
+		printf("ERROR %d GRBwrite(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* Get solution information */
+
+	error = GRBgetintattr(model, GRB_INT_ATTR_STATUS, &optimstatus);
+	if (error) {
+		printf("ERROR %d GRBgetintattr(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* get the objective -- the optimal result of the function */
+	error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL, &objval);
+	if (error) {
+		printf("ERROR %d GRBgettdblattr(): %s\n", error, GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* get the solution - the assignment to each variable */
+	/* 3-- number of variables, the size of "sol" should match */
+	error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, 3, sol);
+	if (error) {
+		printf("ERROR %d GRBgetdblattrarray(): %s\n", error,
+				GRBgeterrormsg(env));
+		return -1;
+	}
+
+	/* print results */
+	printf("\nOptimization complete\n");
+
+	/* solution found */
+	if (optimstatus == GRB_OPTIMAL) {
+		printf("Optimal objective: %.4e\n", objval);
+		printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
+	}
+	/* no solution found */
+	else if (optimstatus == GRB_INF_OR_UNBD) {
+		printf("Model is infeasible or unbounded\n");
+	}
+	/* error or calculation stopped */
+	else {
+		printf("Optimization was stopped early\n");
+	}
+
+	/* IMPORTANT !!! - Free model and environment */
+	GRBfreemodel(model);
+	GRBfreeenv(env);
+
+	return 0;
+}
---
+++ Project/gurobi_func.h
@@ -0,0 +1,13 @@
+/*
+ * gurobi_func.h
+ *
+ *  Created on: Aug 6, 2018
+ *      Author: Lenovo
+ */
+
+#ifndef GUROBI_FUNC_H_
+#define GUROBI_FUNC_H_
+
+
+
+#endif /* GUROBI_FUNC_H_ */
