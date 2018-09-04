42c86ecee49408a3c714bce95fab8e8d6de97947
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-09-04 22:38:27 +0300

    Update MainAux.c

--- Project/MainAux.c
+++ Project/MainAux.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "gameStructs.h"
 
-void printBoard(gameState *metaBoard) {
+void printBoard(gameState *metaBoard) { /*Prints the board for the print_board command*/
 	int i, j;
 	board *playerBoard = metaBoard->gameBoard;
 	for (i = 0; i < playerBoard->cols * playerBoard->rows; i++) {
@@ -48,59 +48,21 @@ void printBoard(gameState *metaBoard) {
 	printf("\n");
 }
 
-void printChanges(int from, int to) {
-	if (from == 0)
+void printChanges(int from, int to) { /*Prints the changes that happened for the undo and redo commands*/
+	if (from == 0) /*If we changed from 0 to something*/
 		printf("_");
 	else
 		printf("%d", from);
 	printf(" to ");
-	if (to == 0)
+	if (to == 0) /*If we changed from something to 0*/
 		printf("_\n");
 	else
 		printf("%d\n", to);
 }
 
-void printForTest(board *playerBoard) {
-	int i, j;
-	for (i = 0; i < playerBoard->cols * playerBoard->rows; i++) {
-		if (i % playerBoard->rows == 0) {
-			for (j = 0;
-					j
-							< playerBoard->rows * playerBoard->cols * 3
-									+ 2 * playerBoard->cols + 1; j++) {
-				printf("-");
-			}
-			printf("\n");
-		}
-		for (j = 0; j < playerBoard->rows * playerBoard->cols; j++) {
-			if (j == 0) {
-				printf("|");
-			}
-			if (playerBoard->board[j][i].fixed)
-				printf(" .%d", playerBoard->board[j][i].value);
-			else {
-				printf("  ");
-				if (playerBoard->board[j][i].value == 0)
-					printf(" ");
-				else
-					printf("%d", playerBoard->board[j][i].value);
-			}
-			if (j % playerBoard->cols == playerBoard->cols - 1)
-				printf(" |");
-		}
-		printf("\n");
-	}
-	for (j = 0;
-			j
-					< playerBoard->rows * playerBoard->cols * 3
-							+ 2 * playerBoard->cols + 1; j++) {
-		printf("-");
-	}
-	printf("\n");
-}
 
 
-void checkInitalize(void *pointer, char *cmd){
+void checkInitalize(void *pointer, char *cmd){ /*Checks if initialization worked or not,and if not prints accordingly and exits*/
 	if (pointer == NULL){
 		printf("Error: %s has failed\n", cmd);
 		exit(0);
