6d07f3cf9cf69fdbed25fdb947e8c24c8cdac29d
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-09-05 17:22:23 +0300

    Update linkedListFunc.h

--- Project/linkedListFunc.h
+++ Project/linkedListFunc.h
@@ -1,7 +1,8 @@
 #ifndef LINKEDLISTFUNC_H_
 #define LINKEDLISTFUNC_H_
 
-void removeAllNext(node *head);
-node* addNextMove(node *currentMove, int *move, int numOfMoves);
+void removeAllNext(node *head);/*Removes the moves after this move from the undo/redo list*/
+node* addNextMove(node *currentMove, int *move, int numOfMoves);/*Adds the new move to the undo/redo list and deletes the moves
+	that come after the current move in the undo/redo list if needed(if they exist)*/
 
 #endif /* LINKEDLISTFUNC_H_ */
