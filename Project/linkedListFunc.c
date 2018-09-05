eb5096d484565385b2f0efc4893036a5ad49e4d9
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-09-05 17:19:20 +0300

    Update linkedListFunc.c

--- Project/linkedListFunc.c
+++ Project/linkedListFunc.c
@@ -3,7 +3,7 @@
 #include "gameStructs.h"
 #include "MainAux.h"
 
-void removeAllNext(node *head) {
+void removeAllNext(node *head) {/*Removes the moves after this move from the undo/redo list*/
 	if (head != NULL) {
 		removeAllNext(head->next);
 		head->next = NULL;
@@ -12,7 +12,8 @@ void removeAllNext(node *head) {
 	}
 }
 
-node* addNextMove(node *currentMove, int *move, int numOfMoves) {
+node* addNextMove(node *currentMove, int *move, int numOfMoves) { /*Adds the new move to the undo/redo list and deletes the moves
+	that come after the current move in the undo/redo list if needed(if they exist)*/
 	int i;
 	node *newMove = (node*) malloc(sizeof(node));
 	checkInitalize(newMove, "malloc");
@@ -23,8 +24,8 @@ node* addNextMove(node *currentMove, int *move, int numOfMoves) {
 		newMove->change[i] = move[i - 1];/*Insert the x y z1 z2 values of the changes to the array of changes in the current move*/
 	}
 	newMove->next = NULL;
-	if (currentMove != NULL) {
-		if (currentMove->next != NULL)
+	if (currentMove != NULL) {/*Is the undo/redo list not empty?*/
+		if (currentMove->next != NULL)/*Is there a next move?*/
 			removeAllNext(currentMove->next);
 		newMove->prev = currentMove;
 		currentMove->next = newMove;
