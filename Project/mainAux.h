b2bff8b186a978f37c44fe50c7bd5080ddd3844d
roypel <31568986+roypel@users.noreply.github.com>
2018-09-11 22:41:48 +0300

    Update mainAux.h

--- Project/mainAux.h
+++ Project/mainAux.h
@@ -3,12 +3,16 @@
 #include "gameStructs.h"
 
 void printBoard(gameState *metaBoard);
-/*The function prints the input board with frame to each block, each cell that is filled is written with fixed cells marked with a dot before their value, and an empty cell is marked with three spaces.
- *INPUT: board *playerBoard - a pointer to a board filled with values ranging from 0 to board length, while 0 indicates an empty cell, and a fixed cell has it's field marked correctly.*/
+/*The function prints the input board with frame for each block, each cell that is filled is written,
+ * with fixed cells marked with a dot before their value, erroneous cells marked with an asterisk and an empty cell is marked with four spaces.
+ *INPUT: gameState *metaBoard - A pointer to a gameState with allocated board and valid values in it, and erroneous and fixed properties of each cell are correctly maintained.*/
 void printChanges(int from, int to);
-/*Prints the changes that happened to the board when we used either the undo or the redo command.From is the number that was in the cell
- before the command,and to is the number that will be in the cell after the command*/
-void checkInitalize(void *pointer, char *cmd);/*A modular command which we use in order to check if initialization of pointers such as in
-commands like malloc has been successfully done or not,and if not then print a message and exit accordingly*/
+/*The function prints the changes made in the board by changing a cell from the value from to the value to.
+ * INPUT: int from - The previous value that was in the cell.
+ *        int to - the new value that is currently in the cell.*/
+void checkInitalize(void *pointer, char *cmd);
+/*The function checks if the pointer was initialized correctly (not NULL) and if it isn't initialized correctly, prints an error message and exits the program.
+ * INPUT: void *pointer - A pointer the function will check if it was initialized properly.
+ *        char *cmd - The command that was issued (calloc, malloc...) that may have failed, used to generate a correct error message.*/
 
 #endif /* MAINAUX_H_ */
