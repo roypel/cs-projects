75f580b43caa959531edd286310a1438ba368cbc
RonDBlade <37776421+RonDBlade@users.noreply.github.com>
2018-09-14 18:05:35 +0300

    Update solver.h

--- Project/solver.h
+++ Project/solver.h
@@ -11,7 +11,8 @@ int findNumSols(board *gameBoard);
  *INPUT: board *gameBoard - A pointer to an allocated gameBoard with valid values.
  *OUTPUT: int representing the amount of solutions the function found.*/
 int fillSingleValues(gameState *metaBoard);
-/*The function tries to find cells that only has a single value available to add to them, and if such a value exists,
+/*The function tries to find cells that only have a single value available to add to them, and if such a value exists,
  * sets the value of the cell to that value, prints an appropriate message and adds all the changes to the undo/redo list. May result in an erroneous board!
- * INPUT: gameState *metaBoard - A pointer to a gameState with an allocated game board with valid values that the function will try to fill "obvious" values to.*/
+ * INPUT: gameState *metaBoard - A pointer to a gameState with an allocated game board with valid values that the function will try to fill "obvious" values to.
+ * OUTPUT: int that represents the amount of cells that only had a single value available to them before we added that value to them*/
 #endif /* SOLVER_H_ */
