f03cfe19b72600f3db96aa27f0a5610ef20febb1
roypel <31568986+roypel@users.noreply.github.com>
2018-09-15 15:58:23 +0300

    Add files via upload

--- Project/game.h
+++ Project/game.h
@@ -1,4 +1,33 @@
-/*Header file which contains the functions that we use in order to execute the different commands of the game*/
+/* Header file which contains the functions that we use in order to execute the different commands of the game. Including the functions:
+ * startPuzzle - The function needs an allocated gameState, a file name (optional in edit mode) of a valid sudoku board, and a valid gameMode.
+ *               The function will try to load the file to the board in gameState and update the relevant fields, and if it successfully did so,
+ *               update the gameState gameMode to the provided gameMode. In edit mode without a file name, an empty 3*3 sudoku board will be loaded by default.
+ * setBoard - The function needs a column and a row of a cell, and a value that if possible will be in the cell after the function finishes, a valid gameState
+ *            with correctly maintained fields for each cell, and a parameter set (0 or 1) which will indicate if the undo/redo list should be updated as well.
+ *            The function will try to enter the value to the specified cell, update the game board cells and undo/redo list if needed.
+ * validate - The function needs a valid gameState with a board already loaded into it.
+ *            The function will check if the board has a valid solution or not, and will print a message accordingly.
+ * generateBoard - The function needs two parameters, the first indicate how many cells we will try to fill into the board, and the second will indicate
+ *                 how many cells will remain with values by the end of the function, and a valid gameState with an empty game board.
+ *                 The function will try to randomly generate a board by randomly filling legal values in the amount of the first parameter,
+ *                 solve the board, and keep values according to the second parameter, printing relevant message in the end of the process.
+ * undo - The function needs a valid gameState with loaded game board and well maintained undo/redo list.
+ *        The function tries to undo the current move in the list (if it exist), reverting all the values from the current game state to the values that were before the current move.
+ * redo - The function needs a valid gameState with loaded game board and well maintained undo/redo list.
+ *        The function tries to redo the last move that was undone (if it exist), reverting all the values from the current game state to the values that were before the undoing.
+ * saveBoard - The function needs a valid gameState with a loaded and well maintained game board, and a file name.
+ *             The function tries to save the current game board to the file given or create it if it doesn't exist. File path may be relative or absolute.
+ * hintBoard - The function needs the column and row of a specific cell and a valid gameState with a loaded game board.
+ *             The function tries to find a value that fit into some solution of the current puzzle, if a solution is possible and the cell is empty, and print it.
+ * numOfSol - The function needs a valid gameBoard with loaded value in it.
+ *            The function finds the amount of different possible solutions to the current game board and prints it.
+ * autofill - The function needs a valid gameState with a loaded game board in it.
+ *            The function fills values to any cell that has only single legal value that can be entered to it in the original board (before it starts filling values).
+ *            May result in an erroneous board afterwards, since values entered by this function aren't taken into account when checking new cells.
+ * resetGame - The function needs a valid gameState with a loaded game board in it and a well maintained undo/redo list.
+ *             The function undoes any move that happened since the loading of the file, and resets the undo/redo list.
+ * exitGame - The function needs a valid and well allocated gameState.
+ *            The function frees any memory that is still in use by the program and exits completely.*/
 
 #ifndef GAME_H_
 #define GAME_H_
